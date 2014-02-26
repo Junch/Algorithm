34566ade21a3aaf873f790c68e3a1ba800555ae6
Jun Chen <Junc76@gmail.com>
2014-02-26 16:04:35 +0800

    Solve the N-Queen problem

---
+++ intellij/src/Ltoj/NQueens.java
@@ -0,0 +1,97 @@
+package Ltoj;
+import java.util.*;
+
+/**
+ * Created by jun on 14-2-26.
+ */
+public class NQueens {
+    public ArrayList<String[]> solveNQueens(int n) {
+        ArrayList<String[]> arr = new ArrayList<String[]>();
+
+        Stack<Node> S = new Stack<Node>();
+        S.push(new Node(0, 0));
+
+        // S holds the valid positions in previous rows
+        while(!S.empty()){
+
+            if (getASolution(S, n))
+                addSolution(S, arr);
+            else if(GetQueenOnNextRow(S, n))
+                continue;
+
+            ChangeQueenToExpand(S, n);
+        }
+
+        return arr;
+    }
+
+    final static class Node{
+        Node(int _x, int _y) {
+            x = _x;
+            y = _y;
+        }
+        int x;
+        int y;
+    };
+
+    private boolean conflict(Stack<Node> S, int x, int y){
+        for (Node t: S){
+            if (t.x == x || t.y == y)
+                return true;
+
+            if (t.x + y == t.y + x ||
+                    t.x + t.y == x + y)
+                return true;
+        }
+
+        return false;
+    }
+
+    private boolean getASolution(Stack<Node> s, int n) {
+        return s.size() == n;
+    }
+
+    private void addSolution(Stack<Node> s, ArrayList<String[]> arr) {
+        int n = s.size();
+        String[] board = new String[n];
+        for (int i=0; i<n; ++i) {
+            Node node = s.get(i);
+            StringBuilder builder = new StringBuilder(n);
+            builder.setLength(n);
+            for (int j=0; j<n; ++j)
+                builder.setCharAt(j, '.');
+            builder.setCharAt(node.x, 'Q');
+            board[i] = builder.toString();
+        }
+
+        arr.add(board);
+    }
+
+    private void ChangeQueenToExpand(Stack<Node> s, int n) {
+        do {
+            Node t = s.pop();
+
+            for (int i = t.x + 1; i < n; ++i) {
+                if (!conflict(s, i, t.y)){
+                    t.x = i;
+                    s.push(t);
+                    return;
+                }
+            }
+        }while(!s.empty());
+    }
+
+    private boolean GetQueenOnNextRow(Stack<Node> s, int n) {
+        Node t = s.lastElement();
+        boolean bGetQueenOnNextRow = false;
+
+        for (int i = 0; i<n; ++i) {
+            if (!conflict(s, i, t.y + 1)) {
+                s.push(new Node(i, t.y + 1));
+                bGetQueenOnNextRow = true;
+                break;
+            }
+        }
+        return bGetQueenOnNextRow;
+    }
+}
---
+++ intellij/src/Ltoj/NQueensTest.java
@@ -0,0 +1,66 @@
+package Ltoj;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.*;
+import java.lang.reflect.*;
+
+/**
+ * Created by jun on 14-2-26.
+ */
+public class NQueensTest {
+
+    NQueens sln = null;
+
+    private Method getConflictMethod() throws NoSuchMethodException {
+        Class[] paras = {Stack.class, int.class, int.class};
+        Method method = sln.getClass().getDeclaredMethod("conflict", paras);
+        method.setAccessible(true);
+        return method;
+    }
+
+    @Before
+    public void setUp() throws Exception{
+        sln = new NQueens();
+    }
+
+    @Test
+    public void testSolveNQueens() throws Exception {
+        ArrayList<String[]> arr = sln.solveNQueens(4);
+        assertEquals(2, arr.size());
+
+        String[] arr0 = new String[]{
+                ".Q..", "...Q", "Q...", "..Q."
+        };
+
+        assertArrayEquals(arr0, arr.get(0));
+    }
+
+    @Test
+    public void conflictWithSameRow() throws Exception {
+        Method method = getConflictMethod();
+
+        Stack<NQueens.Node> S = new Stack<NQueens.Node>();
+        S.push(new NQueens.Node(1, 2));
+        assertEquals(true, method.invoke(sln, S, 1, 3));
+    }
+
+    @Test
+    public void conflictWithSameColumn() throws Exception {
+        Method method = getConflictMethod();
+
+        Stack<NQueens.Node> S = new Stack<NQueens.Node>();
+        S.push(new NQueens.Node(1, 2));
+        assertEquals(true, method.invoke(sln, S, 3, 2));
+    }
+
+    @Test
+    public void conflictWithSlash() throws Exception {
+        Method method = getConflictMethod();
+
+        Stack<NQueens.Node> S = new Stack<NQueens.Node>();
+        S.push(new NQueens.Node(1, 2));
+        assertEquals(true, method.invoke(sln, S, 3, 0));
+        assertEquals(true, method.invoke(sln, S, 3, 4));
+    }
+}

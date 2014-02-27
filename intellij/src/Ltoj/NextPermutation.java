6dd62ec6da494630133a8d51fce1f3e165a30c36
Jun Chen <jun@JundeMac.local>
2014-02-27 21:28:50 +0800

    Next Permutation: Pass the three unittests

--- intellij/src/Ltoj/NextPermutation.java
+++ intellij/src/Ltoj/NextPermutation.java
@@ -1,10 +1,33 @@
 package Ltoj;
 
+import java.util.*;
+
 /**
  * Created by jun on 14-2-27.
  */
 public class NextPermutation {
     public void nextPermutation(int[] num) {
+        int i = num.length - 1;
+        for (; i>0; --i) {
+            if (num[i] > num[i-1]) {
+                break;
+            }
+        }
 
+        if (i != 0) {
+            int j = i + 1;
+            for (; j < num.length; ++j) {
+                if (num[j] < num[i-1]) {
+                    break;
+                }
+            }
+            --j;
+
+            int temp = num[i-1];
+            num[i-1] = num[j];
+            num[j] = temp;
+        }
+
+        Arrays.sort(num, i, num.length);
     }
 }
--- intellij/src/Ltoj/NextPermutationTest.java
+++ intellij/src/Ltoj/NextPermutationTest.java
@@ -8,10 +8,26 @@ import static org.junit.Assert.*;
  */
 public class NextPermutationTest {
     @Test
-    public void testNextPermutation() throws Exception {
+    public void testNextPermutation1() throws Exception {
         NextPermutation sln = new NextPermutation();
         int[] a = new int[]{1, 2, 3};
         sln.nextPermutation(a);
-        assertArrayEquals(new int[]{1,3,2}, a);
+        assertArrayEquals(new int[]{1, 3, 2}, a);
+    }
+
+    @Test
+    public void testNextPermutation2() throws Exception {
+        NextPermutation sln = new NextPermutation();
+        int[] a = new int[]{4, 5, 3};
+        sln.nextPermutation(a);
+        assertArrayEquals(new int[]{5, 3, 4}, a);
+    }
+
+    @Test
+    public void testNextPermutation3() throws Exception {
+        NextPermutation sln = new NextPermutation();
+        int[] a = new int[]{5, 4, 3};
+        sln.nextPermutation(a);
+        assertArrayEquals(new int[]{3, 4, 5}, a);
     }
 }

ca7b6e9d34a153dc2304bd78717511162dfde213
Jun Chen <junc76@gmail.com>
2014-05-11 21:41:24 +0800

    LeetCode: Reverse Words in a String
    Solved with python

--- pycharm/Algorithm/ReverseWords.py
+++ pycharm/Algorithm/ReverseWords.py
@@ -1,7 +1,18 @@
-__author__ = 'chenju'
+import unittest
 
 class Solution:
+    # http://oj.leetcode.com/problems/reverse-words-in-a-string/
     # @param s, a string
     # @return a string
     def reverseWords(self, s):
-        pass
+        words = s.split()
+        words.reverse()
+        return ' '.join(words)
+
+class SolutionTest(unittest.TestCase):
+    def setUp(self):
+        self.Solution = Solution()
+
+    def testReverseWordsGeneral(self):
+        s = self.Solution.reverseWords(" ab  12 ")
+        self.assertEqual(s, "12 ab")
